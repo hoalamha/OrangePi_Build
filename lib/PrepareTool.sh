c461e32878f099cd90ec2a9c2f9e5ed9d72b3801
Hoa Nguyen <hoalamha@gmail.com>
2019-03-31 15:09:39 +0700

    Fix libstdc++.so.6: cannot open shared object file on x64 build system

--- lib/PrepareTool.sh
+++ lib/PrepareTool.sh
@@ -10,5 +10,5 @@ fi
 apt-get -y --no-install-recommends --fix-missing install \
     bsdtar mtools u-boot-tools pv bc \
     gcc automake make curl\
-    lib32z1 lib32z1-dev qemu-user-static \
+    lib32z1 lib32z1-dev qemu-user-static lib32stdc++6\
     dosfstools figlet device-tree-compiler debootstrap
